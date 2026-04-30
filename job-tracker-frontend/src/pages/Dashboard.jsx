a53f775c6ccce71fbdd30e329522f10d651a6f15
Anshul <anshuldeoli833@gmail.com>
2026-04-30 09:40:09 +0530

    fix eslint useEffect dependency

--- job-tracker-frontend/src/pages/Dashboard.jsx
+++ job-tracker-frontend/src/pages/Dashboard.jsx
@@ -9,9 +9,9 @@ export default function Dashboard() {
   const [loading, setLoading] = useState(true);
   const navigate = useNavigate();
 
-  // useEffect(() => {
-  //   fetchData();
-  // },  [fetchData]);
+  useEffect(() => {
+    fetchData();
+  },  []);
 
   const fetchData = async () => {
   const fetchData = useCallback(async () => {
